50db81be102e770a1f256d35c927de0f64f54bf4
GSCrawley <gscrawley@gmail.com>
2021-03-01 23:06:55 -0800

    made and exported world_graph to index

---
+++ assessment/src/world_graph.js
@@ -0,0 +1,265 @@
+import {
+    WIDTH,
+    HEIGHT,
+    MARGINS,
+    ANIMATION_DELAY,
+    ANIMATION_EASING,
+    ANIMATION_DURATION
+  } from "./constants";
+  
+  import Chart from "./chart";
+  
+  class WorldGraph extends Chart {
+    constructor(selector) {
+      super(selector);
+      this.svg = d3.select(selector);
+      this.xAxis();
+      this.yAxis([1, 0], "scaleLinear", 20, () => d3.format(".0%"));
+      this.getData("graphGdp");
+    }
+  
+    getData(metric) {
+      let that = this;
+      d3.json("dist/data/countries.json").then(data => {
+        that.setData(data);
+        that.circles(metric);
+      });
+    }
+  
+    formatOrdinal(num) {
+      const int = parseInt(num),
+        digits = [int % 10, int % 100],
+        ordinals = ['st', 'nd', 'rd', 'th'],
+        oPattern = [1, 2, 3, 4],
+        tPattern = [11, 12, 13, 14, 15, 16, 17, 18, 19];
+      return oPattern.includes(digits[0]) && !tPattern.includes(digits[1])
+        ? int + ordinals[digits[0] - 1]
+        : int + ordinals[3];
+    };
+  
+    circles(metric) {
+      ////////////////////////
+      // tooltip
+      ////////////////////////
+      let tooltip = d3
+        .select("body")
+        .append("div")
+        .style("visibility", "hidden")
+        .attr("class", "tooltip")
+        .style("background-color", "black")
+        .style("border-radius", "5px")
+        .style("padding", "10px")
+        .style("color", "white")
+        .style("z-index", "999999999")
+        .style("position", "absolute")
+        .style("display", "block");
+  
+      let that = this;
+      let showTooltip = function(d) {
+        tooltip.transition().duration(200);
+        tooltip
+          .style("visibility", "visible")
+          .html(
+            `
+    <strong>Country:</strong> ${d.country} (${d.continent})<br/>
+    <strong>Happiness Ranking:</strong> ${that.formatOrdinal(d.ranking)}
+  `
+          )
+          .style("top", d3.event.clientY - 100 + "px")
+          .style("left", d3.event.clientX - 160 + "px");
+      };
+  
+      let moveTooltip = function(d) {
+        showTooltip(d);
+        tooltip
+          .style("top", d3.event.clientY - 100 + "px")
+          .style("left", d3.event.clientX - 160 + "px");
+      };
+  
+      let hideTooltip = function(d) {
+        tooltip
+          .transition()
+          .duration(200)
+          .style("visibility", "hidden");
+      };
+  
+      ////////////////////////
+      // chart rendering
+      ////////////////////////
+      this.chart
+        .selectAll("circle")
+        .data(Object.values(this.data))
+        .enter()
+        .append("circle")
+        .attr(
+          "class",
+          d =>
+            `country ${d.class} continent-${d.continent
+              .split(" ")
+              .join("-")} country-bubble`
+        )
+        .attr("fill", d => {
+          if (d.continent === "Africa") {
+            return "#7cbd1e";
+          } else if (d.continent === "Asia") {
+            return "#ff1f5a";
+          } else if (d.continent === "North America") {
+            return "#303481";
+          } else if (d.continent === "South America") {
+            return "#ff5b44";
+          } else if (d.continent === "Europe") {
+            return "#2fc5cc";
+          } else {
+            return "red";
+          }
+        })
+        .attr("opacity", ".7")
+        .attr("stroke", "#CDCDCD")
+        .attr("stroke-width", "2px")
+        .attr("cx", d => {
+          return this.xScale(d[metric] / 156) + 25;
+        })
+        .on("mouseover", showTooltip)
+        .on("mousemove", moveTooltip)
+        .on("mouseleave", hideTooltip)
+        .transition()
+        .delay((d, i) => i * ANIMATION_DELAY)
+        .duration(ANIMATION_DURATION)
+        .ease(ANIMATION_EASING)
+        .attr("r", d => {
+          if (d.population > 800000000) {
+            return d.population / 25000000;
+          } else if (d.population > 50000000) {
+            return d.population / 10000000;
+          } else if (d.population > 1000000) {
+            return d.population / 1500000;
+          } else {
+            return d.population / 100000;
+          }
+        })
+        .attr("cy", d => {
+          return this.yScale(d.graphRanking / 156);
+        });
+  
+      ////////////////////////
+      // legend
+      ////////////////////////
+      let continents = {
+        AFRICA: { continent: "Africa" },
+        ASIA: { continent: "Asia" },
+        NORTH_AMERICA: { continent: "North America" },
+        SOUTH_AMERICA: { continent: "South America" },
+        EUROPE: { continent: "Europe" }
+      };
+  
+      let continentFocusOn = continentName => {
+        this.chart
+          .selectAll(
+            `circle:not(.continent-${continentName.split(" ").join("-")})`
+          )
+          .attr("opacity", "0.05");
+      };
+  
+      let continentFocusOff = continentName => {
+        this.chart
+          .selectAll(
+            `circle:not(.continent-${continentName.split(" ").join("-")})`
+          )
+          .attr("opacity", "0.7");
+      };
+  
+      let legend = this.chart
+        .selectAll(".legend")
+        .data(Object.values(continents))
+        .enter()
+        .append("g")
+        .attr("class", "legend")
+        .attr("position", "absolute")
+        .attr("transform", `translate(${WIDTH - 130}, ${HEIGHT - 120})`);
+  
+      legend
+        .append("rect")
+        .attr("x", 0)
+        .attr("y", function(d, i) {22
+          return 20 * i;
+        })
+        .attr("width", 20)
+        .attr("height", 20)
+        .style("fill", function(d) {
+          if (d.continent === "Africa") {
+            return "#7cbd1e";
+          } else if (d.continent === "Asia") {
+            return "#ff1f5a";
+          } else if (d.continent === "North America") {
+            return "#303481";
+          } else if (d.continent === "South America") {
+            return "#ff5b44";
+          } else if (d.continent === "Europe") {
+            return "#2fc5cc";
+          } else {
+            return "red";
+          }
+        })
+        .on("mouseover", d => continentFocusOn(d.continent))
+        .on("mouseleave", d => continentFocusOff(d.continent));
+  
+      legend
+        .append("text")
+        .attr("x", 25)
+        .attr("text-anchor", "start")
+        .attr("dy", "1em")
+        .attr("class", d => `legend-${d.continent.split(" ").join("-")}`)
+        .attr("y", function(d, i) {
+          return 20 * i;
+        })
+        .text(function(d) {
+          return d.continent;
+        })
+        .attr("font-size", "12px")
+        .on("mouseover", d => continentFocusOn(d.continent))
+        .on("mouseleave", d => continentFocusOff(d.continent));
+  
+      legend
+        .append("text")
+        .attr("x", 31)
+        .attr("dy", "-.2em")
+        .attr("y", -10)
+        .text("Continent")
+        .attr("font-size", "17px")
+        .style("text-align", "left");
+    }
+  
+    updateData(dataType) {
+      d3.json("dist/data/countries.json").then(data => {
+        this.chart
+          .selectAll(".country-bubble")
+          .transition()
+          .duration(500)
+          .ease(ANIMATION_EASING)
+          .attr("cx", d => {
+            return this.xScale(d[dataType] / 156) + 25;
+          });
+  
+          this.updateAxisLabel(dataType);
+      });
+    }
+  
+    updateAxisLabel(type) {
+      // xLabel
+      let label;
+      if (type === 'graphSocialSupport') {
+        label = 'Social Support';
+      } else if (type === "graphFreedom") {
+        label = 'Freedom';
+      } else if (type === "graphGenerosity") {
+        label = 'Generosity';
+      } else if (type === "graphLifeExpectancy") {
+        label = 'Life Expectancy';
+      } else if (type === 'graphGdp') {
+        label = 'GDP Per Capita ($)';
+      }
+      this.chart.select(".x-axis-label").text(`${label}`);
+    }
+  }
+  
+  export default WorldGraph;
